23477e423bb5c9cae2342154ac7b0fcd092f905d
Michael Fadillah <fadillahmichael@hotmail.com>
2019-09-18 12:21:50 -0700

    init

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+    public Vet(String lastName, String firstName) {
+        super(lastName, firstName);
+    }
+}
